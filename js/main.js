ff6749a794c4ae43111ccd9c84f5d1ed908c7754
Aaron He <aaronhe09@yahoo.com>
2023-06-14 21:56:40 +0000

    assigned and rendered 2 cards to dealer and player

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const $bet = document.querySelector('.bet');
 const $dealButton = document.querySelector('.deal-button');
 const $startingScreenContainer = document.querySelector('.starting-screen-container');
 const $header = document.querySelector('header');
+const $playersCard = document.querySelectorAll('.players-card');
+const $dealersCard = document.querySelectorAll('.dealers-card');
 // game elements
 const $gameContainer = document.querySelector('.game-container');
 const $gameMoney = document.querySelector('.game-money');
@@ -15,22 +17,29 @@ let deckId = null;
 // money variables
 let money = 1000;
 let bet = 0;
-// hands
-const playersHand = null;
-const dealersHand = null;
+//
+const dealersHand = [];
 
 // fetching deck api
-deck.open('GET', 'https://deckofcardsapi.com/api/deck/new/');
+deck.open('GET', 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
 deck.responseType = 'json';
 deck.addEventListener('load', function () {
   deckId = deck.response.deck_id;
 });
 deck.send();
 
-// drawing from deck
-function drawCards(i, whoseHand) {
-  cards.open('GET', `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${i}`);
+// drawing first 4 cardsfrom deck
+function renderFourCards() {
+  cards.open('GET', `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`);
   cards.responseType = 'json';
+  cards.addEventListener('load', function () {
+    $playersCard[0].src = cards.response.cards[0].image;
+    $playersCard[1].src = cards.response.cards[1].image;
+    $dealersCard[1].src = cards.response.cards[3].image;
+    dealersHand.push(cards.response.cards[2]);
+
+    // calculate total value of cards
+  });
   cards.send();
 }
 
@@ -69,4 +78,7 @@ $dealButton.addEventListener('click', function () {
   // render money and bet amount
   $gameMoney.textContent = `Money: $${money}`;
   $gameBet.textContent = `Bet: $${bet}`;
+
+  // draws 4 cards for each player and assign them
+  renderFourCards();
 });
